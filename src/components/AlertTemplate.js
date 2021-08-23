fb2c1de98910307ea78d7ce3305e33de1d51da1e
nhope <nhope@ryerson.ca>
2021-08-23 13:02:59 -0400

    update

--- src/components/AlertTemplate.js
+++ src/components/AlertTemplate.js
@@ -7,7 +7,7 @@ import { InfoCircle } from 'react-bootstrap-icons'
 // close is a function that closes the alert
 const AlertTemplate = ({ style, options, message, close }) =>{
     return (
-        <div className={'.alert'} style={{backgroundColor: 'rgba(255,255,255,0.3)',borderRadius: '10px',padding: '8px 12px',marginTop: '10px'}}>
+        <div className={'.alert'} style={{backgroundColor: 'rgba(255,255,255,0.4)',borderRadius: '10px',padding: '8px 12px',marginTop: '10px'}}>
             <InfoCircle style={{color: 'red',fontSize: '1.5rem', marginRight: '10px'}} />
             <span style={{color: 'black',fontSize: '1.3rem'}} >{message}</span>
         </div >       
